8041b144845e3288630c2316df2076429be98d60
sujjwal21 <ujjwal@adveniotecnosys.tech>
2025-05-17 12:07:26 +0530

    minor fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
 connectDB();
